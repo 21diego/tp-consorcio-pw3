3b31d7524318ecb4e1137ea50a56f078c8428859
Matias <guerreromatias24@gmail.com>
2020-09-27 16:00:24 -0300

    Se agrego el controlador cosorcio y la vista

---
+++ MVC-Web/Controllers/ConsorcioController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_Web.Controllers
+{
+    public class ConsorcioController : Controller
+    {
+        // GET: Consorcio
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
