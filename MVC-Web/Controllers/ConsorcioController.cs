a3e4ace96fc93bb1aa42b8e39a7998dfb71cb97a
Matias <guerreromatias24@gmail.com>
2020-10-04 20:23:03 -0300

    Vistas Index, update consoricio, Entidad y Servicio

--- MVC-Web/Controllers/ConsorcioController.cs
+++ MVC-Web/Controllers/ConsorcioController.cs
@@ -3,15 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Entidades;
+using Servicios;
 
 namespace MVC_Web.Controllers
 {
     public class ConsorcioController : Controller
     {
+        private ServicioConsorcio servicioConsorcio = ServicioConsorcio.getInstance();
+
         // GET: Consorcio
         public ActionResult Index()
         {
-            return View();
+            // nostrar lista consorcios
+            List<Consorcio> consorcios = servicioConsorcio.listarConsorcios();
+
+            return View(consorcios);
+        }
+        
+        public ActionResult update(int id)
+        {
+            Consorcio consorcio = servicioConsorcio.getConsorcioById(id);
+
+            return View(consorcio);
         }
     }
 }
---
+++ Servicios/ServicioConsorcio.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entidades;
+
+namespace Servicios
+{
+    public class ServicioConsorcio
+    {
+        private static ServicioConsorcio instance;
+
+        private ServicioConsorcio() { }
+
+        public static ServicioConsorcio getInstance() {
+            if (instance == null) {
+                instance = new ServicioConsorcio();
+            }
+            return instance;
+        }
+
+        private static List<Consorcio> consorcios = new List<Consorcio>();
+        private static int id = 1;
+
+
+
+
+        public List<Consorcio> listarConsorcios()
+        {
+            consorcios.Add(new Consorcio()
+            {
+                ConsorcioId = id++,
+                Nombre = "La fastina",
+                Ciudad = "gotica",
+                DiaVencimientoExpensas = 5
+
+            }) ;
+            return consorcios;
+        }
+
+        public Consorcio getConsorcioById(int id)
+        {
+            Consorcio consorcio = new Consorcio()
+            {
+                ConsorcioId = id,
+                Nombre = "La fastina",
+                Ciudad = "gotica",
+                DiaVencimientoExpensas = 5
+            };
+            return consorcio;
+        }
+    }
+}
