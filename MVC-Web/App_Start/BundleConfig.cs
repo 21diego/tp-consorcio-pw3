5f04ef134ad2dd267133150cf61ff652404b57b7
pablostrato <pablo_0km@yahoo.com.ar>
2020-10-14 21:02:39 -0300

    se agrego la vista de expensas y template de css

--- MVC-Web/App_Start/BundleConfig.cs
+++ MVC-Web/App_Start/BundleConfig.cs
@@ -22,9 +22,13 @@ namespace MVC_Web
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js"));
 
-            bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+            //bundles.Add(new StyleBundle("~/Content/css").Include(
+                      //"~/Content/bootstrap.css"));
+                     //"~/Content/site.css"));
+
+            bundles.Add(new StyleBundle("~/css").Include(
+                      "~/css/bootstrap.min.css",
+                      "~/css/main.css"));
         }
     }
 }
