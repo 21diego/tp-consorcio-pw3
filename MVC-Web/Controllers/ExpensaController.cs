630d16bc8554ac3bb305926accf1adef1fc6cb7e
pablostrato <pablo_0km@yahoo.com.ar>
2020-10-25 20:12:10 -0300

    commir de prueba

---
+++ MVC-Web/Controllers/ExpensaController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_Web.Controllers
+{
+    public class ExpensaController : Controller
+    {
+        // GET: Expensas
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
