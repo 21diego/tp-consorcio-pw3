2e559ebe989ca4155bd5282b4e7756b9f4b06da6
pablostrato <pablo_0km@yahoo.com.ar>
2020-10-26 00:14:54 -0300

    se crearon las vistas para el ABM de unidades

---
+++ MVC-Web/Controllers/UnidadController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_Web.Controllers
+{
+    public class UnidadController : Controller
+    {
+        // GET: Unidad
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Crear()
+        {
+            return View();
+        }
+
+        public ActionResult Modificar()
+        {
+            return View();
+        }
+    }
+}
