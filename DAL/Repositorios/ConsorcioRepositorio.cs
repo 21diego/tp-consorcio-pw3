51654c56ccd534b5a3143620f23f5ada464a27a3
21diego <morinigodiego21@gmail.com>
2020-11-10 20:06:47 -0300

    mergeo consorcio

---
+++ DAL/Repositorios/ConsorcioRepositorio.cs
@@ -0,0 +1,54 @@
+﻿using DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades.repositorios
+{
+    public class ConsorcioRepositorio
+    {
+        ConsorcioCtx context;
+
+        public ConsorcioRepositorio(ConsorcioCtx contexto)
+        {
+            context = contexto;
+
+        }
+
+        public List<Consorcio> listarConsorcios()
+        {
+            return context.Consorcios.ToList();
+        }
+
+        public void guardarConsorcio(Consorcio consorcio)
+        {
+            context.Consorcios.Add(consorcio);
+            context.SaveChanges();
+        }
+
+        public void eliminarConsorcio(Consorcio consorcio)
+        {
+            context.Consorcios.Remove(consorcio);
+        }
+
+        public Consorcio obtenerConsorcio(int idConsorcio)
+        {
+            return context.Consorcios.Find(idConsorcio);
+        }
+
+        public void editarConsorcio(Consorcio consorcio)
+        {
+            Consorcio consorcioObtenido = context.Consorcios.Find(consorcio.IdConsorcio);
+            consorcioObtenido.Nombre = consorcio.Nombre;
+            consorcioObtenido.IdProvincia = consorcio.IdProvincia;
+            consorcioObtenido.Ciudad = consorcio.Ciudad;
+            consorcioObtenido.Calle = consorcio.Calle;
+            consorcioObtenido.Altura = consorcio.Altura;
+            consorcioObtenido.DiaVencimientoExpensas = consorcio.DiaVencimientoExpensas;
+
+            context.SaveChanges();
+        }
+    }
+}
---
+++ MVC-Web/Controllers/ConsorcioController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DAL;
+using Servicios;
+
+namespace MVC_Web.Controllers
+{
+    public class ConsorcioController : Controller
+    {
+        ConsorcioServicio servicio;
+
+        public ConsorcioController()
+        {
+            ConsorcioCtx context = new ConsorcioCtx();
+            servicio = new ConsorcioServicio(context);
+        }
+
+        // GET: Consorcio
+        public ActionResult Index()
+        {
+            // nostrar lista consorcios
+            List<Consorcio> consorcios = servicio.listarConsorcios();
+
+            return View(consorcios);
+        }
+
+        public ActionResult update(int id)
+        {
+
+            return View();
+        }
+    }
+}
